8228b04e0ed86803c168cf7aea9a419dd839f749
21120447 <haonhat2729@gmail.com>
2024-09-23 10:03:28 +0700

    feat: add list story (home)

--- lib/features/news-feed/screens/new_feed_screen.dart
+++ lib/features/news-feed/screens/new_feed_screen.dart
@@ -1,3 +1,6 @@
+import 'package:facebook_app_ui/features/news-feed/widgets/add_story_card.dart';
+import 'package:facebook_app_ui/features/news-feed/widgets/story_card.dart';
+import 'package:facebook_app_ui/models/story.dart';
 import 'package:facebook_app_ui/models/user.dart';
 import 'package:facebook_app_ui/providers/user_provider.dart';
 import 'package:flutter/material.dart';
@@ -31,6 +34,54 @@ class _NewFeedScreenState extends State<NewsFeedScreen> {
   @override
   Widget build(BuildContext context) {
     final User user = Provider.of<UserProvider>(context).user;
+    final stories = [
+      Story(
+        user: User(
+          name: 'Doraemon',
+          avatar: 'assets/images/user/doraemon.jpg',
+          type: 'page',
+        ),
+        image: ['assets/images/story/1.jpg'],
+        time: ['12 phút'],
+        shareWith: 'public',
+      ),
+      Story(
+        user: User(
+            name: 'Sách Cũ Ngọc', avatar: 'assets/images/user/sachcungoc.jpg'),
+        image: ['assets/images/story/2.jpg'],
+        time: ['3 giờ'],
+        shareWith: 'friends',
+      ),
+      Story(
+        user: User(
+          name: 'Vietnamese Argentina Football Fan Club (VAFFC)',
+          avatar: 'assets/images/user/vaffc.jpg',
+          type: 'page',
+        ),
+        image: ['assets/images/story/3.jpg'],
+        time: ['5 giờ'],
+        shareWith: 'friends-of-friends',
+      ),
+      Story(
+        user: User(
+            name: 'Minh Hương', avatar: 'assets/images/user/minhhuong.jpg'),
+        image: [
+          'assets/images/story/4.jpg',
+          'assets/images/story/5.jpg',
+          'assets/images/story/6.jpg',
+          'assets/images/story/7.jpg',
+        ],
+        video: ['assets/videos/2.mp4', 'assets/videos/1.mp4'],
+        time: ['1 phút'],
+        shareWith: 'friends',
+      ),
+      Story(
+        user: User(name: 'Khánh Vy', avatar: 'assets/images/user/khanhvy.jpg'),
+        video: ['assets/videos/3.mp4'],
+        time: ['1 phút'],
+        shareWith: 'friends',
+      ),
+    ];
 
     return SingleChildScrollView(
       controller: scrollController,
@@ -93,6 +144,38 @@ class _NewFeedScreenState extends State<NewsFeedScreen> {
                   ),
                 ),
               ],
+            ),
+          ),
+          //
+          Container(
+            width: double.infinity,
+            height: 3,
+            color: Colors.black26,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 5,
+                    ),
+                    child: AddStoryCard(),
+                  ),
+                  ...stories.map(
+                    (e) => Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 5,
+                      ),
+                      child: StoryCard(story: e),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ],
